d6219e45ee403c692740482af0a1b3c994996611
kdeneshchuk <kdeneshchuk@kse.org.ua>
2024-11-15 16:48:04 +0200

    Test

--- project.py
+++ project.py
@@ -1,5 +1,11 @@
+import argparse
+
 with open("Olympic Athletes - athlete_events.tsv", "rt") as file:
     next_line = file.readline()
     while next_line:
         next_line = file.readline()
         print(next_line)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("file", help = "Address of the file")
+parser.add_argument("-medals", help)
